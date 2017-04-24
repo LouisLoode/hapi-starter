b257a5db54dff12fdfe8523118eebc751f074fa5
Louis DEBRAINE <louisdebraine@gmail.Com>
2017-04-24 18:14:09 +0200

    Update travis env, fix some little bug

--- config/policies.js
+++ config/policies.js
@@ -3,15 +3,19 @@ import UserModel from '../src/models/user';
 const policies = module.exports = {};
 
 policies.Jwt = (decoded, request, callback) => {
-    console.log('decoded.id = ' + decoded.id);
-    console.log('decoded._id = ' + decoded._id);
-    UserModel.findOne({ _id: decoded.id }).then((user) => {
 
+    UserModel.findOne({ _id: decoded.id }).then((user, err) => {
+        if(err){
+            console.log(err);
+        }
         if (user) {
             request.auth.crendentials = user.toObject();
             return callback(null, true);
         }
-        return callback(null, false);
+        else {
+            return callback(null, false);
+        }
+
     });
 
 };
